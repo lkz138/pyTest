fcf47ef67f87d9605104a0113228fe517e3ee76a
yneon@n138.nws <lent38g-net@yahoo.co.jp>
2020-04-27 11:30:21 +0900

    create

---
+++ Hello.py
@@ -0,0 +1,22 @@
+# ここ参考: https://qiita.com/Fendo181/items/a934e4f94021115efb2e
+
+# Line comment
+"""
+Block comment
+"""
+
+print("Hello py!")
+# セミコロンは無くてもOK
+
+print("Hello py!");
+# セミコロンがあってもOK
+
+# echo は使えないみたいだね
+
+if 0 < 1:
+  print(128)
+
+"""
+ブロック括弧{}はIF文では使えない
+そしてIF()ではないっぽい
+"""
